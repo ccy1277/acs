d198df2603400c5580cc2cde092ebffdd49a2e54
ccy1277 <ccy1277@139.com>
2022-05-08 20:03:53 +0800

    添加Mybatis配置类(分页拦截器)

---
+++ src/main/java/com/ccy1277/acs/config/MybatisConfig.java
@@ -0,0 +1,30 @@
+package com.ccy1277.acs.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Mybatis配置
+ * created by ccy on 2022/5/8
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan({"com.ccy1277.acs.sys.mapper"})
+public class MybatisConfig {
+    // 分页拦截器
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        // 开启 count 的 join 优化,只针对部分 left join
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }
+}
