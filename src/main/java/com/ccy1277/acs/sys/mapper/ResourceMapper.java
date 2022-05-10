634d0fb6241f8729191c5d121953c8827590fc82
ccy1277 <ccy1277@139.com>
2022-05-10 20:41:00 +0800

    添加获取所有可访问资源接口

--- src/main/java/com/ccy1277/acs/sys/mapper/ResourceMapper.java
+++ src/main/java/com/ccy1277/acs/sys/mapper/ResourceMapper.java
@@ -2,15 +2,18 @@ package com.ccy1277.acs.sys.mapper;
 
 import com.ccy1277.acs.sys.model.Resource;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
- * <p>
- *  Mapper 接口
- * </p>
- *
+ * 资源mapper
  * @author ccy1277
  * @since 2022-05-08
  */
 public interface ResourceMapper extends BaseMapper<Resource> {
-
+    /**
+     * 获取用户所有可访问资源
+     */
+    List<Resource> getResourceList(@Param("userId") Long userId);
 }
