c5d009c38db98832b4a96b2bd1edb9c95b7fda41
ccy1277 <ccy1277@139.com>
2022-05-10 20:46:08 +0800

    添加全局异常处理类

---
+++ src/main/java/com/ccy1277/acs/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.ccy1277.acs.common.exception;
+
+import com.ccy1277.acs.common.result.CommonResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 全局异常处理类
+ * created by ccy on 2022/5/9
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // 处理ApiException
+    @ExceptionHandler(value = ApiException.class)
+    @ResponseBody
+    public CommonResult handle(ApiException e) {
+        if (e.getResultCode() != null) {
+            return CommonResult.failed(e.getResultCode());
+        }
+        return CommonResult.failed(e.getMessage());
+    }
+}
