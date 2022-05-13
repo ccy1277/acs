54174b75c734b4ea5b84de0f400c542cc60909e3
ccy1277 <ccy1277@139.com>
2022-05-13 22:15:37 +0800

    实现用户相关信息缓存功能

--- src/main/java/com/ccy1277/acs/sys/service/cache/impl/UserCacheServiceImpl.java
+++ src/main/java/com/ccy1277/acs/sys/service/cache/impl/UserCacheServiceImpl.java
@@ -1,8 +1,11 @@
 package com.ccy1277.acs.sys.service.cache.impl;
 
+import com.ccy1277.acs.common.service.RedisService;
 import com.ccy1277.acs.sys.model.Resource;
 import com.ccy1277.acs.sys.model.User;
 import com.ccy1277.acs.sys.service.cache.UserCacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,23 +16,32 @@ import java.util.List;
  */
 @Service
 public class UserCacheServiceImpl implements UserCacheService {
+    @Autowired
+    private RedisService redisService;
+    @Value("${redis.expire}")
+    private long REDIS_KEY_EXPIRE;
+    @Value("${redis.key.user}")
+    private String REDIS_KEY_ADMIN;
+    @Value("${redis.key.resources}")
+    private String REDIS_KEY_RESOURCES;
+
     @Override
     public User getUser(String username) {
-        return null;
+        return (User)redisService.get(REDIS_KEY_ADMIN + ":" + username);
     }
 
     @Override
     public void setUser(User user) {
-
+        redisService.set(REDIS_KEY_ADMIN + ":" + user.getUsername(), user, REDIS_KEY_EXPIRE);
     }
 
     @Override
     public List<Resource> getResourceList(Long id) {
-        return null;
+        return (List<Resource>) redisService.get(REDIS_KEY_RESOURCES + ":" + id);
     }
 
     @Override
-    public void setResourceList(Long adminId, List<Resource> resources) {
-
+    public void setResourceList(Long id, List<Resource> resources) {
+        redisService.set(REDIS_KEY_RESOURCES + ":" + id, resources, REDIS_KEY_EXPIRE);
     }
 }
