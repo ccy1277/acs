e82f409953dd1514d166dff56070ba3be713ce3d
ccy1277 <ccy1277@139.com>
2022-05-16 22:52:37 +0800

    增加根据用户id查找角色

--- src/main/java/com/ccy1277/acs/sys/mapper/RoleMapper.java
+++ src/main/java/com/ccy1277/acs/sys/mapper/RoleMapper.java
@@ -3,14 +3,16 @@ package com.ccy1277.acs.sys.mapper;
 import com.ccy1277.acs.sys.model.Role;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 /**
- * <p>
- *  Mapper 接口
- * </p>
- *
+ * 角色mapper
  * @author ccy1277
  * @since 2022-05-08
  */
 public interface RoleMapper extends BaseMapper<Role> {
-
+    /**
+     * 查询某用户的角色信息
+     */
+    List<Role> getRolesByUserId(Long userId);
 }
