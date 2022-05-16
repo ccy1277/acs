c2c93430e769d6d73944a05cd5ca36dd18f0aa49
ccy1277 <ccy1277@139.com>
2022-05-16 22:52:08 +0800

    增加根据角色id查找对应菜单

--- src/main/java/com/ccy1277/acs/sys/mapper/MenuMapper.java
+++ src/main/java/com/ccy1277/acs/sys/mapper/MenuMapper.java
@@ -3,14 +3,16 @@ package com.ccy1277.acs.sys.mapper;
 import com.ccy1277.acs.sys.model.Menu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 /**
- * <p>
- *  Mapper 接口
- * </p>
- *
+ * 菜单Mapper接口
  * @author ccy1277
  * @since 2022-05-08
  */
 public interface MenuMapper extends BaseMapper<Menu> {
-
+    /**
+     * 根据角色id获取菜单
+     */
+    List<Menu> getMenuByRoleId(Long roleId);
 }
