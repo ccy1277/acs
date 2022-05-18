263d1af2c05528f0c20afe096d1eb77e902e9e35
ccy1277 <ccy1277@139.com>
2022-05-18 22:42:18 +0800

    增加资源管理功能相关接口

--- src/main/java/com/ccy1277/acs/sys/service/ResourceService.java
+++ src/main/java/com/ccy1277/acs/sys/service/ResourceService.java
@@ -1,8 +1,11 @@
 package com.ccy1277.acs.sys.service;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ccy1277.acs.sys.dto.ResourceDto;
 import com.ccy1277.acs.sys.model.Resource;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * 资源服务接口类
@@ -18,4 +21,24 @@ public interface ResourceService extends IService<Resource> {
      * @return Page<Resource>
      */
     Page<Resource> getResourcePagesByName(String resourceName, Integer pageSize, Integer pageNum);
+
+    /**
+     * 添加资源
+     */
+    boolean addResource(ResourceDto resourceDto);
+
+    /**
+     * 编辑资源信息
+     */
+    boolean updateResource(ResourceDto resourceDto);
+
+    /**
+     * 删除资源信息
+     */
+    boolean deleteResource(Long id);
+
+    /**
+     * 根据资源id查找对应的用户关系
+     */
+    List<Long> getUserRoleRelation(Long resourceId);
 }
--- src/main/java/com/ccy1277/acs/sys/service/impl/ResourceServiceImpl.java
+++ src/main/java/com/ccy1277/acs/sys/service/impl/ResourceServiceImpl.java
@@ -3,11 +3,20 @@ package com.ccy1277.acs.sys.service.impl;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ccy1277.acs.common.exception.Asserts;
+import com.ccy1277.acs.sys.dto.ResourceDto;
 import com.ccy1277.acs.sys.model.Resource;
 import com.ccy1277.acs.sys.mapper.ResourceMapper;
 import com.ccy1277.acs.sys.service.ResourceService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ccy1277.acs.sys.service.cache.UserCacheService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * 资源服务实现类
@@ -16,6 +25,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ResourceServiceImpl extends ServiceImpl<ResourceMapper, Resource> implements ResourceService {
+    @Autowired(required = false)
+    private ResourceMapper resourceMapper;
+    @Autowired
+    private UserCacheService userCacheService;
 
     @Override
     public Page<Resource> getResourcePagesByName(String resourceName, Integer pageSize, Integer pageNum) {
@@ -26,5 +39,43 @@ public class ResourceServiceImpl extends ServiceImpl<ResourceMapper, Resource> i
             wrapper.lambda().like(Resource::getName, resourceName);
         }
         return page(page, wrapper);
+    }
+
+    @Override
+    public boolean addResource(ResourceDto resourceDto) {
+        Resource resource = new Resource();
+        BeanUtils.copyProperties(resourceDto, resource);
+        // 查询资源是否已经存在
+        QueryWrapper<Resource> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(Resource::getName, resource.getName());
+        List<Resource> resources = this.list(wrapper);
+
+        if(resources.size() > 0){
+            Asserts.throwException("资源已存在");
+        }
+        resource.setCreateTime(new Date());
+
+        return this.save(resource);
+    }
+
+    @Override
+    public boolean updateResource(ResourceDto resourceDto) {
+        Resource resource = new Resource();
+        BeanUtils.copyProperties(resourceDto, resource);
+        // 更新了资源信息需要清除缓存
+        userCacheService.deleteResourceListBatch(getUserRoleRelation(resource.getId()));
+        return this.updateById(resource);
+    }
+
+    @Override
+    public boolean deleteResource(Long id) {
+        // 删除了角色需要清除缓存
+        userCacheService.deleteResourceListBatch(getUserRoleRelation(id));
+        return this.removeById(id);
+    }
+
+    @Override
+    public List<Long> getUserRoleRelation(Long resourceId) {
+        return resourceMapper.getUserIdListById(resourceId);
     }
 }
