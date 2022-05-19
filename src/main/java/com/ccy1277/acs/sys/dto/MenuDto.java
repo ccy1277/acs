698196f7d07938f223bb1dc4ffe92a4782793e3c
ccy1277 <ccy1277@139.com>
2022-05-19 16:42:44 +0800

    封装菜单传输对象

---
+++ src/main/java/com/ccy1277/acs/sys/dto/MenuDto.java
@@ -0,0 +1,36 @@
+package com.ccy1277.acs.sys.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
+/**
+ * 菜单传输对象
+ * created by ccy on 2022/5/19
+ */
+@Data
+@ApiModel(value="Menu传输对象", description="菜单")
+public class MenuDto {
+    @NotEmpty(groups = {update.class})
+    @ApiModelProperty(value = "菜单id")
+    private Long id;
+
+    @ApiModelProperty(value = "父级ID")
+    private Long parentId;
+
+    @NotEmpty(groups = {save.class})
+    @ApiModelProperty(value = "菜单名称")
+    private String name;
+
+    @ApiModelProperty(value = "菜单前端图标")
+    private String icon;
+
+    @ApiModelProperty(value = "控制前端是否显示 1:显示 0: 隐藏")
+    private Integer hidden;
+
+    public interface update{}
+    public interface save{}
+}
