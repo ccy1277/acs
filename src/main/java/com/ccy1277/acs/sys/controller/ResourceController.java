5974ca1d1b9908e26eb55f00a153dbf2ce1864aa
ccy1277 <ccy1277@139.com>
2022-05-19 16:42:14 +0800

    增加资源管理控制器方法

--- src/main/java/com/ccy1277/acs/sys/controller/ResourceController.java
+++ src/main/java/com/ccy1277/acs/sys/controller/ResourceController.java
@@ -4,15 +4,14 @@ package com.ccy1277.acs.sys.controller;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ccy1277.acs.common.api.CommonPage;
 import com.ccy1277.acs.common.api.CommonResult;
+import com.ccy1277.acs.sys.dto.ResourceDto;
 import com.ccy1277.acs.sys.model.Resource;
 import com.ccy1277.acs.sys.service.ResourceService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 资源管理功能控制器
@@ -21,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/resource")
+@Api(tags = "ResourceController", description = "资源功能控制器")
 public class ResourceController {
     @Autowired
     private ResourceService resourceService;
@@ -35,6 +35,36 @@ public class ResourceController {
             return CommonResult.success(resources);
         }
         return CommonResult.failed("获取资源列表信息异常");
+    }
+
+    @ApiOperation("创建资源")
+    @PostMapping("/add")
+    public CommonResult addResource(@Validated(ResourceDto.save.class) @RequestBody ResourceDto resourceDto){
+        if(resourceService.addResource(resourceDto)){
+            return CommonResult.success(null, "资源创建成功");
+        }else{
+            return CommonResult.failed("资源创建失败");
+        }
+    }
+
+    @ApiOperation("更新资源")
+    @PostMapping("/edit")
+    public CommonResult editInfo(@Validated(ResourceDto.update.class) @RequestBody ResourceDto resourceDto){
+        if(resourceService.updateResource(resourceDto)){
+            return CommonResult.success(null, "资源更新成功");
+        }else{
+            return CommonResult.failed("资源更新失败");
+        }
+    }
+
+    @ApiOperation("删除指定资源")
+    @PostMapping("/delete/{id}")
+    public CommonResult deleteResource(@PathVariable Long id){
+        if(resourceService.deleteResource(id)){
+            return CommonResult.success(null, "资源删除成功");
+        }else{
+            return CommonResult.failed("资源删除失败");
+        }
     }
 }
 
