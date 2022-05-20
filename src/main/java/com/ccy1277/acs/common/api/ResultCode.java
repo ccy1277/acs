47c6044e2c99060de9573ea298c051d937e0c9c9
ccy1277 <ccy1277@139.com>
2022-05-20 11:02:47 +0800

    更新业务状态码

--- src/main/java/com/ccy1277/acs/common/api/ResultCode.java
+++ src/main/java/com/ccy1277/acs/common/api/ResultCode.java
@@ -1,17 +1,21 @@
 package com.ccy1277.acs.common.api;
 
 /**
- * enum 定义常用的 Code
+ * enum 定义业务状态Code
  * created by ccy on 2022/5/9
  */
 public enum ResultCode implements IResultCode{
-    SUCCESS(200, "success"),
-    FAILED(500, "failed"),
-    VALIDATE_FAILED(404, "validated failed"),
-    LOGIN_FAILED(401, "用户名或密码错误"),
-    UNAUTHORIZED(401, "not logged in or the token has expired"),
-    FORBIDDEN(403, "no relevant permissions"),
-    MSG_NOTFOUND(404, "查询的信息不存在");
+    // 定义业务状态码（通用）
+    SUCCESS(2000, "success"),
+    FAILED(2001, "failed"),
+    VALIDATE_FAILED(2002, "validated failed"),
+    ERROR(2003, "unknown mistake"),
+
+    LOGIN_FAILED(1000, "用户名或密码错误"),
+    UNAUTHORIZED(1001, "用户尚未登录或登录信息过期"),
+    FORBIDDEN(1002, "权限不足"),
+    USER_NOTFOUND(1003, "用户不存在"),
+    MSG_NOTFOUND(1003, "查询的信息不存在");
 
     private Integer code;
     private String msg;
